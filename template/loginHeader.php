ed018cd43eed02371b62d313159507eafa75a10f
kincaidandrew <kincaidandrew2000@outlook.com>
2024-10-29 17:49:06 +0000

    First Commit Ground Zero - Login working

---
+++ template/loginHeader.php
@@ -0,0 +1,8 @@
+<?php 
+session_start();
+// if($_SESSION['Active'] == false){ /* Redirects user to Login.php if not logged in. Remember, we set $_SESSION['Active'] == true in login.php*/
+//     header("location:login.php");
+//     exit;
+// }
+/*the code inside these php tags (i.e. the 5 lines of code above) are required for every page you wish to be accessible only after login*/
+?>
