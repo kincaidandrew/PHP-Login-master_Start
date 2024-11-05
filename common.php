af7f412894a1772d878e414419f73ad82f538f7c
kincaidandrew <kincaidandrew2000@outlook.com>
2024-11-05 17:16:36 +0000

    Ist Commit Steps 1-5 completed

---
+++ common.php
@@ -0,0 +1,7 @@
+<?php
+    function test_input($data) {
+        $data = htmlspecialchars($data, ENT_QUOTES | ENT_SUBSTITUTE, "UTF-8");
+        $data = trim($data);
+        $data = stripslashes($data); 
+        return ($data); 
+    } 
--- public/login.php
+++ public/login.php
@@ -1,7 +1,43 @@
 
 <?php 
 require_once ('config.php'); // This is where the username and password are currently stored (hardcoded in variables)
-require_once('../template/loginHeader.php');//different for log in page to avoid an infinite loop 
+require_once('../templates/loginHeader.php');//different for log in page to avoid an infinite loop 
+
+if (isset($_POST['submit'])) 
+    {
+        require "../common.php";
+        //require "config.php";
+        try { 
+            //$connection = new PDO($dsn, $username, $password, $options);
+            require_once '../src/DBconnect.php';
+            // insert new user code will go here
+        
+            $user_login = array(        
+                "Username" => test_input($_POST['Username']),
+                "Password" => test_input($_POST['Password']),             
+            );
+
+            $sql = sprintf( 
+                "INSERT INTO %s (%s) values (%s)", 
+                "users", 
+                implode(", ",
+            array_keys($user_login)), ":" . implode(", :", array_keys($user_login)) );
+
+            $statement = $connection->prepare($sql);
+            $statement->execute($user_login);
+        } catch(PDOException $error)
+        {
+            echo $sql . "<br>" .$error->getMessage();
+        } 
+    } 
+    // catch(PDOException $error) 
+    // { 
+    //     echo $sql . "<br>" . $error->getMessage();
+    // }
+    require "templates/header.php";
+    if (isset($_POST['submit']) && $statement){ 
+        echo $new_user['firstname']. ' successfully added';
+    } 
 ?>
 
 <!DOCTYPE html>
@@ -33,30 +69,30 @@ require_once('../template/loginHeader.php');//different for log in page to avoid
         <button name="Submit" value="Login" class="button" type="submit">Sign in</button>
 
     </form>
-    <?php
+    <!-- <?php
 
-        /* Check if login form has been submitted */
-        /* isset — Determine if a variable is declared and is different than NULL*/
-        if(isset($_POST['Submit']))
-        {
+        // /* Check if login form has been submitted */
+        // /* isset — Determine if a variable is declared and is different than NULL*/
+        // if(isset($_POST['Submit']))
+        // {
 
-            /* Check if the form's username and password matches */
-            /* these currently check against variable values stored in config.php but later we will see how these can be checked against information in a database*/
-            if( ($_POST['Username'] == $Username) && ($_POST['Password'] == $Password) )
-            {
-                //echo 'Success';
-                /* Success: Set session variables and redirect to protected page */
-                $_SESSION['Username'] = $Username; //store Username to the session
-                $_SESSION['Active'] = true; //remember we can call a session what we like e.g. $_SESSION["newsession"]=$value;
-                header("location:index.php"); /* 'header() is used to redirect the browser */
-                exit; //we’ve just used header() to redirect to another page but we must terminate all current code so that it doesn’t run when we redirect
+        //     /* Check if the form's username and password matches */
+        //     /* these currently check against variable values stored in config.php but later we will see how these can be checked against information in a database*/
+        //     if( ($_POST['Username'] == $Username) && ($_POST['Password'] == $Password) )
+        //     {
+        //         //echo 'Success';
+        //         /* Success: Set session variables and redirect to protected page */
+        //         $_SESSION['Username'] = $Username; //store Username to the session
+        //         $_SESSION['Active'] = true; //remember we can call a session what we like e.g. $_SESSION["newsession"]=$value;
+        //         header("location:index.php"); /* 'header() is used to redirect the browser */
+        //         exit; //we’ve just used header() to redirect to another page but we must terminate all current code so that it doesn’t run when we redirect
             
 
-            }
-            else
-                echo 'Incorrect Username or Password';
-        }
-        ?>
+        //     }
+        //     else
+        //         echo 'Incorrect Username or Password';
+        // }
+        ?> -->
 
 </div>
 </body>
