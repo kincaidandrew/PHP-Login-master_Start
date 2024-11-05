441afc28f01a6f145e66cdc43fd5f71375215405
kincaidandrew <kincaidandrew2000@outlook.com>
2024-11-05 21:08:34 +0000

    log in

---
+++ public/credentials.php
@@ -0,0 +1,10 @@
+<?php
+
+/* Define username and password */
+$Username = "Steve";
+$Password = "pass";
+
+//SQL Select to check if password and username exist if yes log in
+
+// $pdo = new PDO('');
+// $userID = $pdo->query('SELECT id FROM credentials where ')
